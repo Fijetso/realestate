e6958ef6ce2a427dfd028f2073514c6787989266
Fijetso <henrynguyen02081997@gmail.com>
2019-04-17 12:19:37 +0700

    Apply Domain Model & Exception for Not Valid

--- realestate/src/main/java/vn/edu/uit/realestate/Controller/ExceptionResponseResource.java
+++ realestate/src/main/java/vn/edu/uit/realestate/Controller/ExceptionResponseResource.java
@@ -1,8 +1,17 @@
 package vn.edu.uit.realestate.Controller;
 
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import vn.edu.uit.realestate.Model.ExceptionResponse;
 
 @ControllerAdvice
 @RestController
@@ -10,6 +19,12 @@ public class ExceptionResponseResource extends ResponseEntityExceptionHandler {
 
 	public ExceptionResponseResource() {
 		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),request.getDescription(false));
+				return new ResponseEntity<Object>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 
 }
