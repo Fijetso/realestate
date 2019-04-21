f85330e47eabb315dd1c0f4f4f74ebd58b7f1060
thanhd19 <thanhd@nichietsuvn.com>
2019-04-21 11:08:38 +0700

    merge update angular version to 7.3.8 and add package-lock.json to gitignore

--- realestate-client/src/app/app.module.ts
+++ realestate-client/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+// Add module 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule, MatButtonModule} from '@angular/material'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule, MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
