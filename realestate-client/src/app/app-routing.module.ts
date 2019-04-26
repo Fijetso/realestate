c59f958f87331aaf41351e1bd45836594d416d3a
thanhd19 <thanhd@nichietsuvn.com>
2019-04-26 13:55:22 +0700

    add google map and here map

--- realestate-client/src/app/app-routing.module.ts
+++ realestate-client/src/app/app-routing.module.ts
@@ -1,20 +1,39 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { RealestateListComponent } from './components/realestate-list/realestate-list.component';
-import { RealestateItemComponent } from './components/realestate-item/realestate-item.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HereMapComponent } from "./components/here-map/here-map.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RealestateListComponent } from "./components/realestate-list/realestate-list.component";
+import { RealestateItemComponent } from "./components/realestate-item/realestate-item.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { MapModuleComponent } from "./components/map-module/map-module.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'list', pathMatch: 'full' },
-  {path:'list',component: RealestateListComponent},
-  {path:'detail',component: RealestateItemComponent},
-  { path: '**', component: PageNotFoundComponent},
-
+  { path: "", redirectTo: "list", pathMatch: "full" },
+  {
+    path: "list",
+    component: RealestateListComponent,
+    data: { title: "List data page" }
+  },
+  {
+    path: "detail",
+    component: RealestateItemComponent,
+    data: { title: "Detail page" }
+  },
+  { path: "map", component: MapModuleComponent, data: { title: "Map page" } },
+  { path: "login", component: LoginComponent, data: { title: "Login page" } },
+  {
+    path: "**",
+    component: PageNotFoundComponent,
+    data: { title: "Page not found" }
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
-export const routingComponents = [RealestateListComponent,RealestateItemComponent];
+export class AppRoutingModule {}
+export const routingComponents = [
+  RealestateListComponent,
+  RealestateItemComponent
+];
--- realestate-client/src/app/app.module.ts
+++ realestate-client/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule,NO_ERRORS_SCHEMA  } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';   // agm-direction
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { RealestateListComponent } from './components/realestate-list/realestate-list.component';
-import { RealestateItemComponent } from './components/realestate-item/realestate-item.component';
+// import { RealestateListComponent } from './components/realestate-list/realestate-list.component';
+// import { RealestateItemComponent } from './components/realestate-item/realestate-item.component';
 import { RealestateService } from './service/realestate/realestate.service';
 import { MessageService } from './service/message/message.service';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
@@ -16,6 +19,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ContentComponent } from './components/content/content.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { MapModuleComponent } from './components/map-module/map-module.component';
+
+import{ FormsModule} from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { HereMapComponent } from './components/here-map/here-map.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +37,21 @@ import { CarouselComponent } from './components/carousel/carousel.component';
     PageNotFoundComponent,
     ContentComponent,
     CarouselComponent,
+    MapModuleComponent,
+    LoginComponent,
+    HereMapComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCxEsgD2edDcqz5wgKqjYJVjcqmWztNF3A'
+    }),
+    AgmDirectionModule,
+    FormsModule,
+    MatDialogModule
   ],
   providers: [MessageService, RealestateService],
   bootstrap: [AppComponent],
