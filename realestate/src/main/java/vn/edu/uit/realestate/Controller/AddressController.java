b92e0bc30ba8c1c77afc49193a196c2acc5640d5
Fijetso <henrynguyen02081997@gmail.com>
2019-04-28 22:58:42 +0700

    Add AddressController and BookingController

---
+++ realestate/src/main/java/vn/edu/uit/realestate/Controller/AddressController.java
@@ -0,0 +1,47 @@
+package vn.edu.uit.realestate.Controller;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import vn.edu.uit.realestate.Controller.ExceptionHandler.NotFoundException;
+import vn.edu.uit.realestate.Model.Address;
+import vn.edu.uit.realestate.Repository.AddressRepository;
+
+@RestController
+public class AddressController {
+	@Autowired
+	private AddressRepository addressRepository;
+
+    @GetMapping("/addresses")
+    public ResponseEntity<List<Address>> getAddresses() {
+    	List<Address> addresses = (List<Address>) addressRepository.findAll();
+    	if (addresses.isEmpty() == true) {
+    		throw new NotFoundException("Cannot find any Address");
+    	}
+        return new ResponseEntity<>(addresses,HttpStatus.OK);
+    }
+//    @GetMapping("/users/{id}")
+//    public ResponseEntity<User> getUser(@PathVariable long id) {
+//    	Optional<User> foundUser = userRepository.findById(id);
+//		if (foundUser.isPresent()==false) {
+//    		throw new NotFoundException("Cannot find any User with id="+id);
+//    	}
+//        return new ResponseEntity<>(foundUser.get(), HttpStatus.OK);
+//    }
+//    
+//    @DeleteMapping("/users/{id}")
+//    public void deleteUser(@PathVariable long id) {
+//    	if(!userRepository.existsById(id)) {
+//			throw new NotFoundException("Cannot find any User with Id="+id);
+//		}
+//    	userRepository.deleteById(id);
+//    }
+}
---
+++ realestate/src/main/java/vn/edu/uit/realestate/Controller/BookingController.java
@@ -0,0 +1,47 @@
+package vn.edu.uit.realestate.Controller;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import vn.edu.uit.realestate.Controller.ExceptionHandler.NotFoundException;
+import vn.edu.uit.realestate.Model.Booking;
+import vn.edu.uit.realestate.Repository.BookingRepository;
+
+@RestController
+public class BookingController {
+	@Autowired
+	private BookingRepository bookingRepository;
+
+    @GetMapping("/bookings")
+    public ResponseEntity<List<Booking>> getBookings() {
+    	List<Booking> bookings = (List<Booking>) bookingRepository.findAll();
+    	if (bookings.isEmpty() == true) {
+    		throw new NotFoundException("Cannot find any Booking");
+    	}
+        return new ResponseEntity<>(bookings,HttpStatus.OK);
+    }
+    @GetMapping("/bookings/{id}")
+    public ResponseEntity<Booking> getBooking(@PathVariable long id) {
+    	Optional<Booking> foundBooking = bookingRepository.findById(id);
+		if (foundBooking.isPresent()==false) {
+    		throw new NotFoundException("Cannot find any Booking with id="+id);
+    	}
+        return new ResponseEntity<>(foundBooking.get(), HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/bookings/{id}")
+    public void deleteBooking(@PathVariable long id) {
+    	if(!bookingRepository.existsById(id)) {
+			throw new NotFoundException("Cannot find any Booking with Id="+id);
+		}
+    	bookingRepository.deleteById(id);
+    }
+}
