17cdca93353d2f1d3a21f1bc4e2d029be52da811
Fijetso <henrynguyen02081997@gmail.com>
2019-05-10 11:22:47 +0700

    Cloudiary Configuration

--- realestate/src/main/java/vn/edu/uit/realestate/Controller/ImageController.java
+++ realestate/src/main/java/vn/edu/uit/realestate/Controller/ImageController.java
@@ -1,15 +1,28 @@
 package vn.edu.uit.realestate.Controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import vn.edu.uit.realestate.Service.CloudiaryService;
 import vn.edu.uit.realestate.Service.ImageRepository;
 
 @RestController
 public class ImageController {
 	@Autowired
 	private ImageRepository imageRepository;
+	@Autowired
+    private CloudiaryService cloudinaryService;
+
+    @PostMapping("image/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        String url = cloudinaryService.uploadFile(file);
+        return new ResponseEntity<>(url, HttpStatus.OK);
+    }
 //
 //    @GetMapping("/images")
 //    public ResponseEntity<List<Category>> getUserKinds() {
--- realestate/src/main/java/vn/edu/uit/realestate/RealestateApplication.java
+++ realestate/src/main/java/vn/edu/uit/realestate/RealestateApplication.java
@@ -1,15 +1,40 @@
 package vn.edu.uit.realestate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.cloudinary.Cloudinary;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class RealestateApplication {
 
+	@Value("${cloudinary.cloud_name}")
+	private String cloudName;
+
+	@Value("${cloudinary.api_key}")
+	private String apiKey;
+
+	@Value("${cloudinary.api_secret}")
+	private String apiSecret;
+
 	public static void main(String[] args) {
 		SpringApplication.run(RealestateApplication.class, args);
 	}
-
+	@Bean
+	public Cloudinary cloudinaryConfig() {
+		Cloudinary cloudinary = null;
+		Map<String, String> config = new HashMap<String, String>();
+		config.put("cloud_name", cloudName);
+		config.put("api_key", apiKey);
+		config.put("api_secret", apiSecret);
+		cloudinary = new Cloudinary(config);
+		return cloudinary;
+	}
 }
