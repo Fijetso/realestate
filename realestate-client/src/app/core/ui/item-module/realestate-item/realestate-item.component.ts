9e39296ff8ed8b3359e7906d9eeb76d14f88499b
thanhd19 <15520798@gm.uit.edu.vn>
2019-05-15 21:58:22 +0700

    add event handle detail page

--- realestate-client/src/app/core/ui/item-module/realestate-item/realestate-item.component.ts
+++ realestate-client/src/app/core/ui/item-module/realestate-item/realestate-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-realestate-item',
@@ -6,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./realestate-item.component.scss']
 })
 export class RealestateItemComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   mainPic = 'https://picsum.photos/255/150';
-  defaultpic = 'https://dummyimage.com/255x150/333/fff';
+  defaultPic = 'https://dummyimage.com/255x150/333/fff';
+  // defaultPic = '../../../../../assets/images/rolling.svg';
   offset = 100;
+  ngOnInit() {
 
-  ngOnInit() {}
+  }
+  goToPage(url: string){
+    this.router.navigate(['/' + url]);
+  }
 }
