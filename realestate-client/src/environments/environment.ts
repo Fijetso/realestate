c512e5b9eb2bd87a533571e6c60b16f0fcb75e9a
thanhd19 <15520798@gm.uit.edu.vn>
2019-05-23 13:50:24 +0700

    fix conflict

--- realestate-client/src/environments/environment.ts
+++ realestate-client/src/environments/environment.ts
@@ -9,15 +9,6 @@ export const environment = {
     messagingSenderId: '847678221489',
     appId: '1:847678221489:web:985d889d2d396930'
   },
-  firebaseConfig = {
-    apiKey: "AIzaSyDDSFYLHDzTR9_Phma_nPiyF64saTCsJJM",
-    authDomain: "realestate-240713.firebaseapp.com",
-    databaseURL: "https://realestate-240713.firebaseio.com",
-    projectId: "realestate-240713",
-    storageBucket: "realestate-240713.appspot.com",
-    messagingSenderId: "248221268767",
-    appId: "1:248221268767:web:269b9211a53f8abd"
-  };
   ipapi: {
     apiKey: 'e41bcfed8462dbd69772bdb4f6616a24'
   },
