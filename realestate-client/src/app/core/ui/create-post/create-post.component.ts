82deeaacfaf1e4edc4650435171469c650044933
thanhd19 <15520798@gm.uit.edu.vn>
2019-06-01 07:23:48 +0700

    update create post

--- realestate-client/src/app/core/ui/create-post/create-post.component.ts
+++ realestate-client/src/app/core/ui/create-post/create-post.component.ts
@@ -14,21 +14,20 @@ export class CreatePostComponent implements OnInit {
   formPost: any;
   imageUrl = '../../../../assets/images/default.png';
   fileToUpload: File = null;
-  constructor(private http: HttpClient, private api: ApiService) {
+  reKinds: string[] = ['Căn hộ/ Chung cư', 'Nhà riêng', 'Đất nền'];
+  province: any = null;
+  districtList: any = null;
+  wardList: any = null;
+  constructor(private api: ApiService) {
     this.post = {
       name: '',
       email: '',
-      // social: {
-      //   fb: '',
-      //   twt: '',
-      //   web: ''
-      // },
       reKind: 'Căn hộ/ Chung cư',
       dob: '',
       diaChi: {
-        tinh: 'Hồ Chí Minh',
-        huyen: 'Quận 1',
-        xa: 'Xã 1'
+        tinh: 79,
+        huyen: 762,
+        xa: 26800
       },
       price: null,
       currency: 'VNĐ',
@@ -40,44 +39,26 @@ export class CreatePostComponent implements OnInit {
       bluePrints: ''
     };
    }
-  reKinds: string[] = ['Căn hộ/ Chung cư', 'Nhà riêng', 'Đất nền'];
-  states: State[] = [
-    {name: 'Quận 1'},
-    {name: 'Quận 2'},
-    {name: 'Quận 3'},
-    {name: 'Quận 4'},
-    {name: 'Quận 5'},
-    {name: 'Quận 6'},
-    {name: 'Quận 7'},
-    {name: 'Quận 8'},
-    {name: 'Quận 9'},
-    {name: 'Quận 10'},
-    {name: 'Quận 11'},
-    {name: 'Quận 12'},
-    {name: 'Quận Bình Thạnh'},
-    {name: 'Quận Gò Vấp'},
-    {name: 'Quận Tân Bình'},
-    {name: 'Quận Tân Phú'},
-    {name: 'Quận Bình Tân'},
-    {name: 'Huyện Nhà Bè'},
-    {name: 'Huyện Bình Chánh'},
-    {name: 'Huyện Hóc Môn'},
-    {name: 'Huyện Củ Chi'},
-    {name: 'Huyện Cần Giờ'},
-  ];
-  xaList = [
-    'Xã 1',
-    'Xã 2',
-    'Xã 3',
-    'Xã 4',
-    'Xã 5',
-    'Xã 6',
-    'Xã 7',
-    'Xã 8',
-    'Xã 9',
-    'Xã 10'
-  ];
+
   ngOnInit() {
+    this.getProvince(79);
+  }
+  getProvince(provinceId: number) {
+    this.api.getProvincesById(provinceId).subscribe(province => {
+      this.province = province;
+      this.getDistrictFromProvince(provinceId);
+    });
+  }
+
+  getDistrictFromProvince(provinceId: number) {
+    this.api.getDistrictFromProvinceId(provinceId).subscribe(districtList => {
+      this.districtList = districtList;
+    });
+  }
+  getWardFromDisctrictid(provinceId: number, districtId: number) {
+    this.api.getWardFromDistrictId(provinceId, districtId).subscribe(wardList => {
+      this.wardList = wardList;
+    });
   }
   onFileChange(event) {
     if (event.target.files.length > 0) {
@@ -85,6 +66,8 @@ export class CreatePostComponent implements OnInit {
       this.formPost.get('bluePrints').setValue(file);
     }
   }
+
+
   private prepareSave(): any {
     const input = new FormData();
     input.append('avatar', this.formPost.get('bluePrints').value);
