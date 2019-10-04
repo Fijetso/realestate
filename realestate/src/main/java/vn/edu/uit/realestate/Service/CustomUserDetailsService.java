7a5abe0d4ce7e1e96ec9d9a7df35ca43bfe9e65f
Fijetso <henrynguyen02081997@gmail.com>
2019-10-04 08:36:49 +0700

    Exception for Inactive Account

--- realestate/src/main/java/vn/edu/uit/realestate/Service/CustomUserDetailsService.java
+++ realestate/src/main/java/vn/edu/uit/realestate/Service/CustomUserDetailsService.java
@@ -23,12 +23,15 @@ public class CustomUserDetailsService implements UserDetailsService{
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> optionalUser = userRepository.findByName(username);
-		optionalUser.orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+		Optional<User> optionalUser = userRepository.findByEmail(username);
+		optionalUser.orElseThrow(() -> new UsernameNotFoundException("Email not found"));
 //		optionalUser
 //		.ifPresent(user ->  new CustomUserDetails(user));
 		///khong hieu khuc nay
 		User user = optionalUser.get();
+		if(user.isActive()==false) {
+				throw new RuntimeException("The Account hasn't activated yet");
+		}
 		List<GrantedAuthority> authorities = new ArrayList<>();
 		user.getRoles().forEach(authority ->
 			authorities.add(new SimpleGrantedAuthority("ROLE_"+authority.getName()))
