cb73b061ef166db7338072e94a98835a1c2a4220
Fijetso <henrynguyen02081997@gmail.com>
2019-11-07 10:43:12 +0700

    Successfull Configure MySQL

--- realestate/src/main/java/vn/edu/uit/realestate/RealestateApplication.java
+++ realestate/src/main/java/vn/edu/uit/realestate/RealestateApplication.java
@@ -8,18 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 import com.cloudinary.Cloudinary;
 
 import vn.edu.uit.realestate.Common.Common;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {Neo4jDataAutoConfiguration.class, DataSourceAutoConfiguration.class})
 @ComponentScan(value = {Common.Constains.MAIN_PACKAGE})
-@EnableAutoConfiguration(exclude = {Neo4jDataAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class RealestateApplication {
 
 	@Value("${cloudinary.cloud_name}")
--- realestate/src/main/java/vn/edu/uit/realestate/Relational/Model/Security/ConfirmationToken.java
+++ realestate/src/main/java/vn/edu/uit/realestate/Relational/Model/Security/ConfirmationToken.java
@@ -21,17 +21,15 @@ public class ConfirmationToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="token_id")
     private long tokenid;
 
-    @Column(name="confirmation_token")
     private String confirmationToken;
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
 
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = false, name = "user_id")
+    @JoinColumn(nullable = false, name = "userId")
     private User user;
 
     public ConfirmationToken() {
