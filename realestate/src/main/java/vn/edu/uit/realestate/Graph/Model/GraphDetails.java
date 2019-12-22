46aaab6588f028acfc04150510af8ea7024ea7a6
Fijetso <henrynguyen02081997@gmail.com>
2019-12-22 15:01:21 +0700

    Update postTrade

--- realestate/src/main/java/vn/edu/uit/realestate/Graph/Model/GraphDetails.java
+++ realestate/src/main/java/vn/edu/uit/realestate/Graph/Model/GraphDetails.java
@@ -51,6 +51,7 @@ public class GraphDetails {
 	}
 
 	public void setLength(Long length) {
+		if(length !=null)
 		this.length = length;
 	}
 
@@ -59,6 +60,7 @@ public class GraphDetails {
 	}
 
 	public void setWidth(Long width) {
+		if(width !=null)
 		this.width = width;
 	}
 
@@ -67,6 +69,7 @@ public class GraphDetails {
 	}
 
 	public void setSquare(Long square) {
+		if(square !=null)
 		this.square = square;
 	}
 
