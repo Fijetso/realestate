85743343a74efe90cabd02dc9813bfda4ee05539
danhthanh418 <15520798@gm.uit.edu.vn>
2019-12-31 23:44:44 +0700

    config success ck editor

--- realestate-client/src/app/app.module.ts
+++ realestate-client/src/app/app.module.ts
@@ -59,7 +59,7 @@ import { HttpLinkModule } from 'apollo-angular-link-http';
 import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
 import { GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import {CKEditorModule} from 'ngx-ckeditor';
 
 import { RealEstateWrapperComponent } from './components/real-estate/real-estate-wrapper/real-estate-wrapper.component';
 import { AlertComponent } from './core/modal/alert/alert.component';
--- realestate-client/src/app/core/ui/create-post/create-post.component.ts
+++ realestate-client/src/app/core/ui/create-post/create-post.component.ts
@@ -1,30 +1,31 @@
 import { GraphQueryService } from './../../../services/graphql/graph-query.service';
 import { RealEstate } from './../../../model/real-estate/real-estate';
 import { ApiService } from './../../../services/api/api.service';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { State } from '../home-page/marketting/marketting.component';
-
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
   styleUrls: ['./create-post.component.scss']
 })
 export class CreatePostComponent implements OnInit {
- 
   reList: RealEstate[];
-  constructor(private api: ApiService,private graphql : GraphQueryService) {
-    this.graphql.getAllTrade();
+  public editorValue = '';
+  constructor(private api: ApiService, private graphql: GraphQueryService) {
+    // this.graphql.getAllTrade();
   }
 
   ngOnInit() {
-    this.reList= this.api.getAllRE();
+    // this.reList= this.api.getAllRE();
   }
 
-  delete(id: any){
-    let result = confirm('Do you want delelte it ?');
-    if(result){
-      this.api.deleteRE(id);
-    }
+  // delete(id: any){
+  //   let result = confirm('Do you want delelte it ?');
+  //   if(result){
+  //     this.api.deleteRE(id);
+  //   }
+  // }
+
+  onSubmit() {
+    console.log(this.editorValue);
   }
 }
