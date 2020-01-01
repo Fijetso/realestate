52b4f51e3751dff21d34a9207046785aeb0757d7
danhthanh418 <15520798@gm.uit.edu.vn>
2020-01-02 01:18:07 +0700

    format fake news date

--- realestate-client/src/app/core/ui/news-module/news-detail/fakedata.ts
+++ realestate-client/src/app/core/ui/news-module/news-detail/fakedata.ts
@@ -9,31 +9,31 @@ export const newsData = [
         </head>
         <body>
         <p><strong>Vay ng&acirc;n h&agrave;ng l&agrave; đ&ograve;n bẩy t&agrave;i ch&iacute;nh gi&uacute;p người mua nh&agrave; nhanh ch&oacute;ng hiện thực h&oacute;a giấc mơ an cư, nhất l&agrave; với những người c&oacute; thu nhập thấp hoặc vốn t&iacute;ch lũy mỏng. Nhưng l&agrave;m thế n&agrave;o để vay mua nh&agrave; an to&agrave;n v&agrave; c&oacute; l&atilde;i suất thấp?</strong></p>
-        
+
         <p><img alt="Bí quyết vay mua nhà lãi suất thấp và an toàn nhất, ảnh 1" src="http://res.cloudinary.com/fijetso3671/image/upload/v1577897209/RealEstate/kqv0noicwrmx9o7fw37s.webp" style="height:447px; width:800px" /></p>
-        
+
         <p>Khi khao kh&aacute;t c&oacute;&nbsp;nh&agrave; ri&ecirc;ng&nbsp;v&agrave; chọn biện ph&aacute;p vay mua nh&agrave;, kh&ocirc;ng &iacute;t người tặc lưỡi m&igrave;nh sẽ thắt chặt mọi chi ti&ecirc;u, cố gắng c&agrave;y cuốc để trả nợ sớm. Tuy nhi&ecirc;n, việc duy tr&igrave; cuộc sống kham khổ l&acirc;u d&agrave;i để &ldquo;c&otilde;ng&rdquo; nợ kh&ocirc;ng hề đơn giản. Hơn nữa, trong cuộc sống c&oacute; thể xảy ra nhiều biến cố như trong nh&agrave; c&oacute; người ốm đau, c&ocirc;ng việc trắc trở, l&agrave;m ăn kh&oacute; khăn&hellip; Thực tế, kh&ocirc;ng &iacute;t trường hợp kh&ocirc;ng t&iacute;nh to&aacute;n kỹ, khiến khoản vay mua nh&agrave; trở th&agrave;nh g&aacute;nh nặng về t&agrave;i ch&iacute;nh, thậm ch&iacute; sa lầy trong nợ, phải b&aacute;n nh&agrave; trả nợ hoặc bị ng&acirc;n h&agrave;ng siết nợ&hellip; B&agrave;i to&aacute;n l&atilde;i suất v&agrave; thời gian vay ph&ugrave; hợp cũng g&acirc;y kh&oacute; cho nhiều người, đặc biệt l&agrave; những người chưa c&oacute; kinh nghiệm vay ng&acirc;n h&agrave;ng.</p>
-        
-        <p>Nhằm gi&uacute;p người mua nh&agrave; c&oacute; được khoản vay đảm bảo an to&agrave;n t&agrave;i ch&iacute;nh với l&atilde;i suất thấp, Propzy c&oacute; dịch vụ hỗ trợ tư vấn t&agrave;i ch&iacute;nh miễn ph&iacute; cho mỗi kh&aacute;ch h&agrave;ng. Đặc biệt, với ch&iacute;nh s&aacute;ch ưu đ&atilde;i của c&aacute;c ng&acirc;n h&agrave;ng d&agrave;nh cho kh&aacute;ch h&agrave;ng của Propzy, người vay mua nh&agrave; sẽ được hưởng mức l&atilde;i suất thấp hơn so với thị trường.</p>
-        
+
+        <p>Nhằm gi&uacute;p người mua nh&agrave; c&oacute; được khoản vay đảm bảo an to&agrave;n t&agrave;i ch&iacute;nh với l&atilde;i suất thấp, REVN c&oacute; dịch vụ hỗ trợ tư vấn t&agrave;i ch&iacute;nh miễn ph&iacute; cho mỗi kh&aacute;ch h&agrave;ng. Đặc biệt, với ch&iacute;nh s&aacute;ch ưu đ&atilde;i của c&aacute;c ng&acirc;n h&agrave;ng d&agrave;nh cho kh&aacute;ch h&agrave;ng của REVN, người vay mua nh&agrave; sẽ được hưởng mức l&atilde;i suất thấp hơn so với thị trường.</p>
+
         <h2><strong>Thiết kế khoản vay mua nh&agrave; đảm bảo an to&agrave;n t&agrave;i ch&iacute;nh</strong></h2>
-        
+
         <p>Theo c&aacute;c chuy&ecirc;n gia t&agrave;i ch&iacute;nh của REVN, th&ocirc;ng thường, với những người c&oacute; mức thu nhập trung b&igrave;nh kh&aacute; (dưới 30 triệu/th&aacute;ng), mức trả nợ/thu nhập mỗi th&aacute;ng khoảng 50-60% l&agrave; ổn. C&ograve;n với những người c&oacute; mức thu nhập cao (tr&ecirc;n 40 triệu/th&aacute;ng), c&oacute; thể t&iacute;nh to&aacute;n khoản vay để sao cho mức trả nợ/thu nhập mỗi th&aacute;ng khoảng 70%. Tuy nhi&ecirc;n, tr&ecirc;n thực tế, t&igrave;nh h&igrave;nh t&agrave;i ch&iacute;nh, mức độ chi ti&ecirc;u của mỗi gia đ&igrave;nh kh&ocirc;ng giống nhau. Chẳng hạn, nếu gia đ&igrave;nh c&oacute; con nhỏ, c&oacute; cha mẹ gi&agrave; thường xuy&ecirc;n đau ốm mỗi khi tr&aacute;i gi&oacute; trở trời, mức trả nợ/thu nhập sẽ cần t&iacute;nh to&aacute;n để giảm đi cho ph&ugrave; hợp. Hoặc khi mức thu nhập của người vợ/chồng kh&ocirc;ng cố định, cũng cần điều chỉnh lại khoản vay. Nếu &aacute;p dụng một c&aacute;ch cứng nhắc, người vay mua nh&agrave; c&oacute; thể phải đối mặt với những kh&oacute; khăn về t&agrave;i ch&iacute;nh trong thời gian trả nợ.</p>
-        
+
         <p>Tại REVN, c&aacute;c chuy&ecirc;n gia t&agrave;i ch&iacute;nh am hiểu về vay vốn mua nh&agrave; v&agrave; nhiều năm kinh nghiệm sẽ thiết kế khoản vay d&agrave;nh ri&ecirc;ng cho mỗi kh&aacute;ch h&agrave;ng dựa tr&ecirc;n những điều kiện thực tế, t&iacute;nh to&aacute;n khả năng trả nợ để đảm bảo an to&agrave;n t&agrave;i ch&iacute;nh, gi&uacute;p người vay mua nh&agrave; y&ecirc;n t&acirc;m khi hiện thực h&oacute;a giấc mơ an cư của m&igrave;nh.</p>
-        
+
         <h2><strong>L&atilde;i suất thấp hơn thị trường nhờ ch&iacute;nh s&aacute;ch ưu đ&atilde;i d&agrave;nh ri&ecirc;ng</strong></h2>
-        
+
         <p>Hiện tại, REVN đang hợp t&aacute;c với khoảng 50 ng&acirc;n h&agrave;ng, trong đ&oacute; c&oacute; nhiều ng&acirc;n h&agrave;ng lớn như BIDV, Vietcombank, Agribank, VIB, Techcombank&hellip; Người vay mua nh&agrave; c&oacute; thể thoải m&aacute;i lựa chọn ng&acirc;n h&agrave;ng v&agrave; chi nh&aacute;nh ph&ugrave; hợp để thuận tiện nhất cho việc giao dịch, li&ecirc;n hệ. Đặc biệt, người mua nh&agrave; sẽ được vay với l&atilde;i suất thấp hơn thị trường nhờ ch&iacute;nh s&aacute;ch ưu đ&atilde;i của c&aacute;c ng&acirc;n h&agrave;ng d&agrave;nh ri&ecirc;ng cho c&aacute;c kh&aacute;ch h&agrave;ng của REVN.</p>
-        
+
         <p><img alt="Vay mua nhà ảnh 2" src="http://res.cloudinary.com/fijetso3671/image/upload/v1577897963/RealEstate/smq7ukb20q9eya0sy6dq.webp" style="height:1192px; width:2000px" /></p>
-        
+
         <p>Ngo&agrave;i ra, người vay mua nh&agrave; sẽ được c&aacute;c chuy&ecirc;n gia t&agrave;i ch&iacute;nh của REVN hỗ trợ xử l&yacute; c&aacute;c ph&aacute;t sinh sau vay (chẳng hạn như người vay muốn đổi t&agrave;i sản đảm bảo, người vay muốn d&ugrave;ng sổ tiết kiệm để chứng minh t&agrave;i ch&iacute;nh&hellip;)</p>
-        
+
         <p><em><strong>Một số ưu điểm kh&aacute;c của Dịch vụ tư vấn t&agrave;i ch&iacute;nh tại REVN:</strong></em></p>
-        
+
         <ul>
-            <li>Bạn sẽ được tư vấn tận nơi nếu c&oacute; nhu cầu. Mọi hỗ trợ dịch vụ tư vấn t&agrave;i ch&iacute;nh l&agrave; 0 đồng, ngay cả khi bạn kh&ocirc;ng mua nh&agrave; tại Propzy</li>
+            <li>Bạn sẽ được tư vấn tận nơi nếu c&oacute; nhu cầu. Mọi hỗ trợ dịch vụ tư vấn t&agrave;i ch&iacute;nh l&agrave; 0 đồng, ngay cả khi bạn kh&ocirc;ng mua nh&agrave; tại REVN</li>
             <li>Thời gian ra ph&ecirc; duyệt vay vốn chỉ trong v&ograve;ng 01 ng&agrave;y, ngay khi bạn cung cấp đầy đủ hồ sơ theo y&ecirc;u cầu</li>
             <li>Quy tr&igrave;nh v&agrave; thời gian xử l&yacute; hồ sơ sẽ được chuy&ecirc;n gia t&agrave;i ch&iacute;nh th&ocirc;ng b&aacute;o trước với bạn v&agrave; cam kết thực hiện ch&iacute;nh x&aacute;c</li>
             <li>Bạn kiểm so&aacute;t được tiến độ vay vốn v&agrave; lu&ocirc;n được chuy&ecirc;n gia của REVN giải đ&aacute;p c&aacute;c thắc mắc trong suốt thời gian l&agrave;m thủ tục</li>
@@ -48,4 +48,4 @@ export const newsData = [
             name: ' 360 Bất động sản'
         }
     }
-]
+];
