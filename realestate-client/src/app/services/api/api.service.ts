8005ac3441b01bfe023099e8ead61676223fb6d5
danhthanh418 <15520798@gm.uit.edu.vn>
2020-01-04 15:59:15 +0700

    change object to any

--- realestate-client/src/app/services/api/api.service.ts
+++ realestate-client/src/app/services/api/api.service.ts
@@ -113,15 +113,15 @@ export class ApiService {
   }
   // Address service
   getProvinces() {
-    return this.http.get<Address>(this.addressURL);
+    return this.http.get<any>(this.addressURL);
   }
 
   getDistrictFromProvinceId(provinceId: number) {
-    return this.http.get<Address>(this.addressURL + provinceId + '/districts' );
+    return this.http.get<any>(this.addressURL + provinceId + '/districts' );
   }
 
   getWardFromDistrictId(provinceId: number, districtId: number) {
-    return this.http.get<Address>(this.addressURL + provinceId + '/districts/' + districtId + '/wards');
+    return this.http.get<any>(this.addressURL + provinceId + '/districts/' + districtId + '/wards');
   }
   // get Favorite trade order by fav count
   getFavRealEstate() {
