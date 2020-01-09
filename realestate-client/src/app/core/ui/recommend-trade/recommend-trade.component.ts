0f4d7d5164d6ad0646d0b1a9965d43b52b27e333
danhthanh418 <15520798@gm.uit.edu.vn>
2020-01-09 17:36:19 +0700

    apply ui recommend by search

--- realestate-client/src/app/core/ui/recommend-trade/recommend-trade.component.ts
+++ realestate-client/src/app/core/ui/recommend-trade/recommend-trade.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from './../../../services/api/api.service';
 
 @Component({
   selector: 'app-recommend-trade',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recommend-trade.component.scss']
 })
 export class RecommendTradeComponent implements OnInit {
+  tradeRecommendList: any;
 
-  constructor() { }
+  constructor(private api: ApiService) {
+    this.getTradeRecommend('Kinh doanh', [760, 774], [50, 75, 50, 100, 50, 50], [1, 2, 3, 3, 3]).subscribe(res => {
+      this.tradeRecommendList = res;
+      console.log(this.tradeRecommendList);
+    })
+   }
 
   ngOnInit() {
   }
 
+  getTradeRecommend(job, districtIdList, squareList, priceList) {
+    return this.api.getTradeRecommend(job, districtIdList, squareList, priceList);
+  }
 }
