f974bd09030c576e9ed9e5b8381e56e0790c6f11
danhthanh418 <15520798@gm.uit.edu.vn>
2020-01-12 12:23:28 +0700

    change style format news

--- realestate-client/src/app/core/ui/news-module/news-module.module.ts
+++ realestate-client/src/app/core/ui/news-module/news-module.module.ts
@@ -7,17 +7,28 @@ import { NewsDetailComponent } from './news-detail/news-detail.component';
 import { NewsItemComponent } from './news-item/news-item.component';
 import { MatCardModule, MatButtonModule, MatDividerModule } from '@angular/material';
 import { FilterNewsPipe } from '../../../ultility/pipe/filter-news/filter-news.pipe';
+import { SafeHtmlPipe } from '../../../ultility/pipe/safe-html/safe-html.pipe';
 
 
 @NgModule({
-  declarations: [NewsListComponent, NewsDetailComponent, NewsItemComponent, FilterNewsPipe],
+  declarations: [NewsListComponent,
+    NewsDetailComponent,
+    NewsItemComponent,
+    FilterNewsPipe,
+    SafeHtmlPipe
+  ],
   imports: [
-    CommonModule,
+  CommonModule,
     NewsModuleRoutingModule,
     MatCardModule,
     MatButtonModule,
     MatDividerModule
   ],
-  exports: [NewsListComponent, NewsDetailComponent, NewsItemComponent, FilterNewsPipe],
+  exports: [NewsListComponent,
+    NewsDetailComponent,
+    NewsItemComponent,
+    FilterNewsPipe,
+    SafeHtmlPipe
+  ],
 })
 export class NewsModuleModule { }
